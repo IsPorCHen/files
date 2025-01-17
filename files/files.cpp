49b497dd40f8af38b806dd6c1f18dd46604e26af
IsPorCHen <fazy2019@gmail.com>
2025-01-17 22:49:52 +0500

    files

---
+++ files/files.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct Baggage {
+    int numItems;
+    double weight;
+};
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    ifstream inputFile("Bagazh.txt");
+
+    if (!inputFile) {
+        cerr << "Ошибка при открытии файла!" << endl;
+        return 1;
+    }
+
+    vector<Baggage> passengers;
+
+    Baggage baggage;
+    while (inputFile >> baggage.numItems >> baggage.weight) {
+        passengers.push_back(baggage);
+    }
+
+    inputFile.close();
+
+    cout << "Пассажиры с багажом из одной вещи, вес которой меньше 5 кг:" << endl;
+    for (const auto& passenger : passengers) {
+        if (passenger.numItems == 1 && passenger.weight < 5.0) {
+            cout << "Количество вещей: " << passenger.numItems
+                << ", Вес: " << passenger.weight << " кг" << endl;
+        }
+    }
+
+    return 0;
+}
