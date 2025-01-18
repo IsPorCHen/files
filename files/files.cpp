4c2303a97f122adb1da75e7ea9dc0cab2b299283
student1 <cvv_code@yandex.ru>
2025-01-18 10:15:25 +0500

    asdf

--- files/files.cpp
+++ files/files.cpp
@@ -1,39 +1,41 @@
-﻿#include <iostream>
-#include <fstream>
-#include <vector>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <locale.h>
 
 using namespace std;
 
-struct Baggage {
-    int numItems;
-    double weight;
-};
+FILE* openFile(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        wprintf(L"Не удалось открыть файл\n");
+        exit(1);
+    }
+    return file;
+}
+
+void readAndPrintData(FILE* file) {
+    wchar_t fio[100], position[50], Otch[50], job[20], birthdate[20], address[200], phone[20];
+
+    while (fwscanf(file, L"%ls %ls %ls %ls %ls %ls %ls", fio, position, Otch, job, birthdate, address, phone) != EOF) {
+        wcout << L"ФИО: " << fio << L" " << position << L" " << Otch << endl;
+        wcout << L"Должность: " << job << endl;
+        wcout << L"Дата рождения: " << birthdate << endl;
+        wcout << L"Адрес: " << address << endl;
+        wcout << L"Телефон: " << phone << endl;
+        wcout << L"----------------------------" << endl;
+    }
+}
 
 int main() {
-    setlocale(LC_ALL, "RU");
-    ifstream inputFile("Bagazh.txt");
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    const char* filename = "Bagazh.txt";
 
-    if (!inputFile) {
-        cerr << "Ошибка при открытии файла!" << endl;
-        return 1;
-    }
+    FILE* file = openFile(filename);
 
-    vector<Baggage> passengers;
+    readAndPrintData(file);
 
-    Baggage baggage;
-    while (inputFile >> baggage.numItems >> baggage.weight) {
-        passengers.push_back(baggage);
-    }
-
-    inputFile.close();
-
-    cout << "Пассажиры с багажом из одной вещи, вес которой меньше 5 кг:" << endl;
-    for (const auto& passenger : passengers) {
-        if (passenger.numItems == 1 && passenger.weight < 5.0) {
-            cout << "Количество вещей: " << passenger.numItems
-                << ", Вес: " << passenger.weight << " кг" << endl;
-        }
-    }
+    fclose(file);
 
     return 0;
 }
